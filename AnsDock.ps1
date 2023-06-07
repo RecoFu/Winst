4d1c722340381c9698f13b5addaa5b6e586b6eec
Reco <RecoFu@users.noreply.github.com>
2023-06-07 16:04:20 +0800

    Update AnsDock.ps1

--- AnsDock.ps1
+++ AnsDock.ps1
@@ -1,26 +1,26 @@
-# 檢查是否已安裝 Docker Desktop
+# Check if Docker Desktop is installed
 if (-not (Get-Command -Name docker -ErrorAction SilentlyContinue)) {
-    Write-Host "正在安裝 Docker Desktop..."
+    Write-Host "Installing Docker Desktop..."
     Invoke-WebRequest -UseBasicParsing -Uri https://desktop.docker.com/win/stable/Docker%20Desktop%20Installer.exe -OutFile DockerDesktopInstaller.exe
     Start-Process -Wait -FilePath .\DockerDesktopInstaller.exe
     Remove-Item -Path .\DockerDesktopInstaller.exe
 }
 
-# 檢查 Docker 服務是否正在執行
+# Check if Docker service is running
 $dockerService = Get-Service -Name docker -ErrorAction SilentlyContinue
 if (-not $dockerService) {
-    Write-Host "正在啟動 Docker 服務..."
+    Write-Host "Starting Docker service..."
     Start-Service -Name docker
 }
 
-# 安裝所需的套件
+# Install required packages
 Install-PackageProvider -Name NuGet -Force
 Install-Module -Name DockerMsftProvider -Force
 
-# 下載官方 Python 映像檔
+# Download official Python image
 docker pull python:3.9
 
-# 建立 Dockerfile
+# Create Dockerfile
 $dockerfile = @'
 FROM python:3.9
 RUN apt-get update && apt-get install -y \
@@ -32,11 +32,11 @@ RUN pip install ansible
 
 $dockerfile | Out-File -FilePath Dockerfile -Encoding UTF8
 
-# 建立 Ansible 映像檔
+# Build Ansible image
 docker build -t ansible-docker .
 
-# 清理中間檔案
+# Clean up intermediate files
 Remove-Item -Path Dockerfile
 
-# 啟動 Ansible 容器
+# Start Ansible container
 docker run -it --rm ansible-docker

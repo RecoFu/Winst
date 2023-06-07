9fcab9bec41d08eca250f2c161d9f6fd5f75f516
Reco <RecoFu@users.noreply.github.com>
2023-06-07 22:16:31 +0800

    Update AnsDock.ps1

--- AnsDock.ps1
+++ AnsDock.ps1
@@ -1,28 +1,32 @@
-# 检查是否安装了 Docker Desktop
+# 检查是否已安装 Docker Desktop
 if (-not (Get-Command -Name docker -ErrorAction SilentlyContinue)) {
-    Write-Host Installing Docker Desktop...
+    Write-Host "正在安装 Docker Desktop..."
     Invoke-WebRequest -UseBasicParsing -Uri "https://desktop.docker.com/win/stable/Docker%20Desktop%20Installer.exe" -OutFile DockerDesktopInstaller.exe
-    Start-Process -Wait -FilePath .\DockerDesktopInstaller.exe
-    Remove-Item -Path .\DockerDesktopInstaller.exe
+    Start-Process -Wait -FilePath ".\DockerDesktopInstaller.exe"
+    Remove-Item -Path ".\DockerDesktopInstaller.exe"
 }
 
-# 检查 Docker 服务是否正在运行
+# 检查 Docker 服务是否存在
 $dockerService = Get-Service -Name docker -ErrorAction SilentlyContinue
 if (-not $dockerService) {
-    Write-Host Starting Docker service...
-    Start-Service -Name docker
+    Write-Host "找不到名为 'docker' 的服务。请确保 Docker 服务已正确安装并正在运行。"
+    exit
 }
 
-# 安装所需的软件包
+# 启动 Docker 服务
+Write-Host "正在启动 Docker 服务..."
+Start-Service -Name docker
+
+# 安装所需的套件
 Install-PackageProvider -Name NuGet -Force
 Install-Module -Name DockerMsftProvider -Force
 
-# 下载官方的 Python 镜像
-docker pull python3.9
+# 下载官方 Python 映像
+docker pull python:3.9
 
 # 创建 Dockerfile
 $dockerfile = @'
-FROM python3.9
+FROM python:3.9
 RUN apt-get update && apt-get install -y \
     openssh-client \
     sshpass \

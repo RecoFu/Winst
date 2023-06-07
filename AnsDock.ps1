49c272451c5f550f69d8bca1d71aabd66eda70d7
Reco <RecoFu@users.noreply.github.com>
2023-06-07 21:07:20 +0800

    Update AnsDock.ps1

--- AnsDock.ps1
+++ AnsDock.ps1
@@ -1,16 +1,16 @@
-# Check if Docker Desktop is installed
+# Check if Docker Desktop is installed or upgrade it
 if (-not (Get-Command -Name docker -ErrorAction SilentlyContinue)) {
-    Write-Host "Installing Docker Desktop..."
-    Invoke-WebRequest -UseBasicParsing -Uri https://desktop.docker.com/win/stable/Docker%20Desktop%20Installer.exe -OutFile DockerDesktopInstaller.exe
-    Start-Process -Wait -FilePath .\DockerDesktopInstaller.exe
-    Remove-Item -Path .\DockerDesktopInstaller.exe
+    Write-Host "Installing or upgrading Docker Desktop..."
+    Invoke-WebRequest -UseBasicParsing -Uri "https://desktop.docker.com/win/stable/Docker%20Desktop%20Installer.exe" -OutFile "DockerDesktopInstaller.exe"
+    Start-Process -Wait -FilePath ".\DockerDesktopInstaller.exe"
+    Remove-Item -Path ".\DockerDesktopInstaller.exe"
 }
 
 # Check if Docker service is running
-$dockerService = Get-Service -Name docker -ErrorAction SilentlyContinue
+$dockerService = Get-Service -Name "Docker" -ErrorAction SilentlyContinue
 if (-not $dockerService) {
     Write-Host "Starting Docker service..."
-    Start-Service -Name docker
+    Start-Service -Name "Docker"
 }
 
 # Install required packages
@@ -30,13 +30,13 @@ RUN apt-get update && apt-get install -y \
 RUN pip install ansible
 '@
 
-$dockerfile | Out-File -FilePath Dockerfile -Encoding UTF8
+$dockerfile | Out-File -FilePath "Dockerfile" -Encoding UTF8
 
 # Build Ansible image
 docker build -t ansible-docker .
 
 # Clean up intermediate files
-Remove-Item -Path Dockerfile
+Remove-Item -Path "Dockerfile"
 
 # Start Ansible container
 docker run -it --rm ansible-docker

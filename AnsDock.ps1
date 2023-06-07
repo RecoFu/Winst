11675bba2d4b670def677e44b86c865ada219f8e
Reco <RecoFu@users.noreply.github.com>
2023-06-07 22:24:49 +0800

    Update AnsDock.ps1

--- AnsDock.ps1
+++ AnsDock.ps1
@@ -1,4 +1,4 @@
-# Check if Docker Desktop is installed
+# 檢查是否已安裝 Docker Desktop
 if (-not (Get-Command -Name docker -ErrorAction SilentlyContinue)) {
     Write-Host "Installing Docker Desktop..."
     Invoke-WebRequest -UseBasicParsing -Uri "https://desktop.docker.com/win/stable/Docker%20Desktop%20Installer.exe" -OutFile DockerDesktopInstaller.exe
@@ -6,41 +6,37 @@ if (-not (Get-Command -Name docker -ErrorAction SilentlyContinue)) {
     Remove-Item -Path ".\DockerDesktopInstaller.exe"
 }
 
-# Check if Docker service is running
+# 檢查 Docker 服務是否正在運行
 $dockerService = Get-Service -Name docker -ErrorAction SilentlyContinue
 if (-not $dockerService) {
-    Write-Host "Docker service not found. Please ensure that Docker service is installed and running."
-    exit
+    Write-Host "Starting Docker service..."
+    Start-Service -Name docker
 }
 
-# Start Docker service
-Write-Host "Starting Docker service..."
-Start-Service -Name docker
-
-# Install required packages
+# 安裝所需的套件
 Install-PackageProvider -Name NuGet -Force
 Install-Module -Name DockerMsftProvider -Force
 
-# Download official Python image
+# 下載官方 Python 映像檔
 docker pull python:3.9
 
-# Create Dockerfile
-$dockerfile = @'
+# 建立 Dockerfile
+$dockerfile = @"
 FROM python:3.9
 RUN apt-get update && apt-get install -y \
     openssh-client \
     sshpass \
     && rm -rf /var/lib/apt/lists/*
 RUN pip install ansible
-'@
+"@
 
 $dockerfile | Out-File -FilePath Dockerfile -Encoding UTF8
 
-# Build Ansible image
+# 建立 Ansible 映像檔
 docker build -t ansible-docker .
 
-# Clean up intermediate files
+# 清理中間檔案
 Remove-Item -Path Dockerfile
 
-# Start Ansible container
+# 啟動 Ansible 容器
 docker run -it --rm ansible-docker
